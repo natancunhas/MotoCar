06f0d8702d877f9b709aab4df56554c94b26f9ac
Natan Cunha <104222003+natancunhas@users.noreply.github.com>
2026-09-24 16:05:56 -0300

    Update moto.js

--- moto.js
+++ moto.js
@@ -1,11 +1,15 @@
-class Moto {
-    constructor(modelo, ano, serie){
-        this.modelo = modelo;
-        this.ano = ano;
-        this.serie = serie;
-    }
-}
-const moto = new Moto("Honda 160cc", 2020, "Urban");
-console.log(moto.modelo);
-console.log(moto.ano);   
-console.log(moto.serie);   
+        class Moto {
+            constructor(modelo, ano, serie, versao){
+                this.modelo = modelo;
+                this.ano = ano;
+                this.serie = serie;
+                this.versao = versao; 
+            }
+        }
+        const moto = new Moto("Honda 160cc", 2020, "Urban", "Bigtrail");
+        console.log(moto.modelo);
+        console.log(moto.ano);   
+        console.log(moto.serie); 
+        console.log(moto.versao);  
+        
+        
